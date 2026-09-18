c8e9dd6bd74655e18557374d29a8e282f759e86d
LARISSA  BERTLING <81658334+larissabertling@users.noreply.github.com>
2026-09-18 09:14:42 -0300

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -1,8 +1,7 @@
 import logging
 import os
-
-import azure.functions as func
 import requests
+import azure.functions as func
 
 app = func.FunctionApp()
 
@@ -11,47 +10,65 @@ app = func.FunctionApp()
     schedule="0 */1 * * * *",
     arg_name="myTimer",
     run_on_startup=False,
-    use_monitor=False,
+    use_monitor=False
 )
 def TimerLogOnly(myTimer: func.TimerRequest) -> None:
-    logging.info("[TimerLogOnly] Timer trigger executado com sucesso!")
+    logging.info("Timer executado com sucesso!")
 
 
-@app.route(route="HttpEcho", methods=["GET"], auth_level=func.AuthLevel.ANONYMOUS)
+@app.route(
+    route="HttpEcho",
+    methods=["GET"],
+    auth_level=func.AuthLevel.ANONYMOUS
+)
 def HttpEcho(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info("[HttpEcho] Requisicao HTTP recebida.")
+
+    logging.info("Requisição HTTP recebida.")
 
     nome = req.params.get("nome")
 
     if not nome:
         return func.HttpResponse(
             "Envie um parametro 'nome' na URL. Exemplo: ?nome=Joao",
-            status_code=400,
+            status_code=400
         )
 
-    mensagem = f"Voce enviou: {nome} -- processado pela HttpEcho!"
-    logging.info(f"[HttpEcho] {mensagem}")
+    mensagem = f"Voce enviou: {nome} - processado pela HttpEcho!"
 
-    return func.HttpResponse(mensagem, status_code=200)
+    logging.info(mensagem)
+
+    return func.HttpResponse(
+        mensagem,
+        status_code=200
+    )
 
 
-
+# Timer que chama a função HTTP
 @app.timer_trigger(
     schedule="0 */2 * * * *",
     arg_name="myTimer2",
     run_on_startup=False,
-    use_monitor=False,
+    use_monitor=False
 )
 def TimerCallsHttp(myTimer2: func.TimerRequest) -> None:
-    logging.info("[TimerCallsHttp] Iniciando chamada HTTP para a HttpEcho...")
 
-    base_url = os.environ.get("HTTP_ECHO_URL", "http://localhost:7071/api/HttpEcho")
-    parametro_enviado = "TAPRA-2026"
+    logging.info("Iniciando chamada HTTP para a HttpEcho...")
+
+    url = os.environ.get(
+        "HTTP_ECHO_URL",
+        "http://localhost:7071/api/HttpEcho"
+    )
+
+    nome = "TAPRA-2026"
 
     try:
-        response = requests.get(
-            base_url, params={"nome": parametro_enviado}, timeout=10
+        resposta = requests.get(
+            url,
+            params={"nome": nome},
+            timeout=10
         )
-        logging.info(f"[TimerCallsHttp] Resposta recebida: {response.text}")
+
+        logging.info(f"Resposta recebida: {resposta.text}")
+
     except Exception as erro:
-        logging.error(f"[TimerCallsHttp] Erro ao chamar a HttpEcho: {erro}")
+        logging.error(f"Erro ao chamar a HttpEcho: {erro}")
